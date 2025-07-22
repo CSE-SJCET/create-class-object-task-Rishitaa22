0a30301a4fcd002f43ce00461650e46f1062b628
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2025-07-22 18:14:07 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+class Student {
+     String name;
+     int rollNumber;
+     char grade;
 
-    // TODO: Create a method to display student details
+     public void displayDetails() {
+             System.out.println("Name: " + name);
+             System.out.println("Roll Number: " + rollNumber);
+             System.out.println("Grade: " + grade);
+     }
+}
 public class Main {
       public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
+        Student student = new Student();
+           student.name = "Rishita";
+           student.rollNumber = 63;
+           student.grade = "A";
+           
+           student.displayDetails();
     }
 }
